f285f39c05491efbadfd268f3d76ec4083f7dc67
Gurpreet Sanjay Kukkar <kukkar@usc.edu>
2026-03-24 02:05:52 -0700

    Use environment variable for API URL

--- frontend/src/components/QueryBox.js
+++ frontend/src/components/QueryBox.js
@@ -1,7 +1,7 @@
 import { Search } from "lucide-react";
 import axios from "axios";
 
-const API = "http://localhost:8000/api";
+const API = (process.env.REACT_APP_API_URL || "http://localhost:8000") + "/api";
 
 function QueryBox({ query, setQuery, setResults, setThemes, setLoading, loading }) {
   
